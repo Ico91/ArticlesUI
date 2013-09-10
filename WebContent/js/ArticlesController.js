23b58be1f0e933a27e73d760548ac6622600a56c
Hristo Danovski <hr.danovski@gmail.com>
2013-09-10 14:44:29 +0300

    Added comments

--- WebContent/js/ArticlesController.js
+++ WebContent/js/ArticlesController.js
@@ -1,7 +1,13 @@
+/**
+ * Manages communication between the ArticlesListController & ArticleDetailsController
+ */
 function ArticlesController() {
 	var articlesListController = new ArticlesListController(this);
 	var articleDetailsController = new ArticleDetailsController(this);
 	
+	/**
+	 * Loads the necessary html contents
+	 */
 	this.init = function() {
 		$("#container").load('articles.html', function() {
 			articlesListController.init();
@@ -9,19 +15,30 @@ function ArticlesController() {
 			$('.darken').hide();
 		});
 	};
-	
+	/**
+	 * Invoked when creating a new article, tells the article details controller to show it.
+	 */
 	this.onNew = function() {
 		articleDetailsController.show(null);
 	};
 
+	/**
+	 * Invoked on selection an article from the list, tells the article details controller to show it
+	 */
 	this.onSelect = function(article) {
 		articleDetailsController.show(article);
 	};
 
+	/**
+	 * Invoked when deleting an article from the list, tells the article details controller to update the contents
+	 */
 	this.onDelete = function(article) {
 		articleDetailsController.articleDeleted(article);
 	};
 
+	/**
+	 * Invoked when saving an article, tells the articles list controller to update the list.
+	 */
 	this.onSave = function() {
 		articlesListController.refresh();
 	};
--- WebContent/js/MainController.js
+++ WebContent/js/MainController.js
@@ -1,9 +1,18 @@
+/**
+ * Manages communication between the UserSessionController and the LoginController
+ */
 function MainController() {
+	/**
+	 * Creates a new instance of the login controller
+	 */
 	this.init = function() {
 		var loginController = new LoginController(this);
 		loginController.init();
 	};
 
+	/**
+	 * Invoked when the user successfully logs in
+	 */
 	this.userLoggedIn = function(user) {
 		var userSessionController = new UserSessionController(this);
 		userSessionController.init();
